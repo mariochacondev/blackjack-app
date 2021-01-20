1f83d0a2b702aa4bedeedb4e1205d6cddb7f183d
mariodevweb <mariojosech@gmail.com>
2021-01-20 11:15:49 +0100

    3

--- components/Page3.js
+++ components/Page3.js
@@ -3,6 +3,6 @@ import {StyleSheet, Text, Button, View} from 'react-native';
 
 export default function Page3(props) {
     return <View>
-        <Text>Page3</Text>
+        <Text>Page3 PLAY</Text>
     </View>
 }
