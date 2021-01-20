db6f83b1c1a298f9ed29218359ae85e7934ee562
mariodevweb <mariojosech@gmail.com>
2021-01-20 11:05:57 +0100

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,35 +4,32 @@ import {StyleSheet, Button, Text, View} from 'react-native';
 export default function Navbar(props){
 
 
-    const pages = ["Page1", "Page2", "Page3", "Page4"];
-    const listItems = pages.map((page) =>
-  <Button title={page} onPress={handlePress}/>
-);
+    let buttons = props.pages.map(function(page) {
 
-    const[changePage, setPage] = useState(false);
+        return <Button color="black" title={page.title} onPress={function () {
 
-    const handlePress = function(){
-    setPage(!changePage)
+            props.setIdPage(page.id)
 
-    }
+        }} />
 
+    });
 
+    
 
-
-
-    return <View style={StyleSheet.navbar}>
-
-        {listItems}
+    return ( 
+    <View style={styles.navbar} >
+        {buttons}
     </View>
-
+ );
 }
 
-const style = StyleSheet.create({
-    navbar: {
-        height: 40,
-        width: '100%',
-        backgroundColor: "#0055FF",
-        flexDirection: 'row',
-        justifyContent: "space-around"
-    }
+const styles = StyleSheet.create({
+
+navbar: {
+    width: '100%',
+    color:"black",
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    },    
+
 })
