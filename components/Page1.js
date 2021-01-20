a1b7aa8a46352293114e77a22e2c27660280d520
mariodevweb <mariojosech@gmail.com>
2021-01-20 11:15:32 +0100

    1

--- components/Page1.js
+++ components/Page1.js
@@ -3,6 +3,6 @@ import {StyleSheet, Text, Button, View} from 'react-native';
 
 export default function Page1(props) {
     return <View>
-        <Text>Table 1 here</Text>
+        <Text>Table 1 here PLAY</Text>
     </View>
 }
