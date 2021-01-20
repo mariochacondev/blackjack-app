6b503c0ad062f47801687adf8e1c7035a7746adb
mariodevweb <mariojosech@gmail.com>
2021-01-20 10:56:34 +0100

    app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { StyleSheet, Button, Text, View, ImagePropTypes } from 'react-native';
 
 import Header from './components/Header';
@@ -10,24 +10,29 @@ import Page3 from './components/Page3';
 
 export default function App() {
 
-  const[changeText, setText] = useState(false);
+  const [ idPage, setIdPage] = useState("1")
+  const pages = [
+    {
+      id:"1",
+      title:"PAGE 1",
+      component:<Page1/>,
+    },
+    {
+      id:"2",
+      title:"PAGE 2",
+      component:<Page2/>,
+    },
+    {
+      id:"3",
+      title:"PAGE 3",
+      component:<Page3/>,
+    },
 
-  const handlePress = function(){
-    setText(!changeText)
-  
-  }
-  const[changeText2, setText2] = useState(false);
+  ]
 
-  const handlePress2 = function(){
-    setText2(!changeText2)
-  
-  }
-  const[changeText3, setText3] = useState(false);
-
-  const handlePress3 = function(){
-    setText3(!changeText3)
-  
-  }
+  let body = pages.find(function(page) {
+    return page.id === idPage
+  })
   
   
   return (
@@ -35,47 +40,22 @@ export default function App() {
       <Header title="Blackjack App" />
       
 
-      <View style={styles.body}>
+      <Navbar pages={pages} setIdPage={setIdPage} />
 
-      <Navbar />
-
-
-      { changeText === true &&
-        <Text>Page1</Text>
-      }
-      { changeText2 === true &&
-        <Text>Page2</Text>
-      }
-      { changeText3 === true &&
-        <Text>Page3</Text>
-      }
-        <Button title={changeText === false ? "Afficher Page 1" : "Cacher Page 1"} 
-        onPress={handlePress}/>
-        <Button title={changeText2 === false ? "Afficher Page 2" : "Cacher Page 2"} 
-        onPress={handlePress2}/>
-        <Button title={changeText3 === false ? "Afficher Page 3" : "Cacher Page 3"} 
-        onPress={handlePress3}/>
+      {body.component}
+        
       
-      </View>
+      
       <StatusBar style="auto" />
-    </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-  },
-
-  body: {
-    height: "100%",
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    flexDirection: 'row',
   },
 
 });
