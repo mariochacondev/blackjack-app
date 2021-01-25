246492482f9f55d66757a9cafc01dd5b9db2a1c3
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-01-25 09:33:33 +0100

    creation structure bouton start

--- components/Page1.js
+++ components/Page1.js
@@ -4,5 +4,9 @@ import {StyleSheet, Text, Button, View} from 'react-native';
 export default function Page1(props) {
     return <View>
         <Text>Table 1 here PLAY</Text>
+        <View>
+            <button class="btn-start" title="start">Start</button>
+        </View>
     </View>
 }
+
