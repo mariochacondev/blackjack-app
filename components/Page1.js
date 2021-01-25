7428bf85a076af80079b179c10d50e594da7f315
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-01-25 10:09:02 +0100

    Rectification bouton start

--- components/Page1.js
+++ components/Page1.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, Text, Button, View} from 'react-native';
 
 export default function Page1(props) {
+    const[start, setStart] = useState('start');
+
+    const handlePress1 = function (){
+        setStart('Start')
+    }
     return <View>
         <Text>Table 1 here PLAY</Text>
         <View>
-            <button class="btn-start" title="start">Start</button>
+            <Button style={styles.btn-start} onPress={handlePress1} title="Start"/>
         </View>
     </View>
 }
 
+const styles = StyleSheet.create({
+
+})
